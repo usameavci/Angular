46913e8e9b05623a7904b7aa52f03664256b9575
Luke Freeman <luke.freeman@jjmarketing.co.uk>
2015-06-12 15:20:29 +0100

    Auto deployment

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+user="freemandigital"
+userDir="/home/freemandigital/"
+domain="freemandigital.com"
+repo="git@bitbucket.org:freemandigital/test.git"
 env=$1
 
-if [ $1 = "live" ]; then
-	ssh git@freemandigital.com "/deploy.sh git@bitbucket.org:freemandigital/test.git master freemandigital.com freemandigital"
-fi
-			
-if [ $1 = "dev" ]; then
-	ssh git@freemandigital.com "/deploy.sh git@bitbucket.org:staging.freemandigital/test.git master freemandigital.com freemandigital"
-fi
+	### push to git repo ---------------------------------------------------------
+	git add .
+	git commit -am 'Auto deployment'
+	git push origin master
+
+	### deploy to server ---------------------------------------------------------
+	if [ $1 = "live" ]; then
+		ssh git@freemandigital.com "/deploy.sh $repo master $domain $user"
+	fi
+
+	if [ $1 = "dev" ]; then
+		ssh git@freemandigital.com "/deploy.sh $repo master $domain $user"
+	fi
+
+
+echo -e "+-----------------------------------------+"
+echo -e "| Site successfully deployed!             |"
+echo -e "+-----------------------------------------+"
