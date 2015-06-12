039abc0f697d2828a33e7808a0f02c98fec64045
Luke Freeman <luke.freeman@jjmarketing.co.uk>
2015-06-12 13:34:08 +0100

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ssh git@freemandigital.com "/deploy.sh git@bitbucket.org:freemandigital/test.git master freemandigital.com freemandigital"
+env=$1
+
+if [ $1 = "live" ]; then
+	ssh git@freemandigital.com "/deploy.sh git@bitbucket.org:freemandigital/test.git master freemandigital.com freemandigital"
+fi
+			
+if [ $1 = "dev" ]; then
+	ssh git@freemandigital.com "/deploy.sh git@bitbucket.org:staging.freemandigital/test.git master freemandigital.com freemandigital"
+fi
